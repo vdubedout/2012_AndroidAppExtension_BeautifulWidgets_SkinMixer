880e59f2cccb6405c5d4223edea4f84090496138
Vincent Dubedout <vincent@dubedout.eu>
2012-10-02 20:48:02 +0200

    add enum skinPart

---
+++ SkinMixer/src/com/andexp/skinmixer/fragment/SkinPartType.java
@@ -0,0 +1,33 @@
+package com.andexp.skinmixer.fragment;
+
+public enum SkinPartType {
+	BACKGROUND(0),
+	BACKGROUND_NUMBERS(1), 
+	NUMBER_0(2), 
+	NUMBER_1(3), 
+	NUMBER_2(4), 
+	NUMBER_3(5), 
+	NUMBER_4(6), 
+	NUMBER_5(7), 
+	NUMBER_6(8), 
+	NUMBER_7(9), 
+	NUMBER_8(10), 
+	NUMBER_9(11), 
+	DOTS(12), 
+	AM(13), 
+	PM(14), 
+	PREVIEW(15), 
+	TEXTFILE(16), 
+	FIRST_SKIN_IMAGE(BACKGROUND.getValue()), 
+	LAST_SKIN_IMAGE(PM.getValue()),
+	VOID(100);
+	
+	private int value;
+	SkinPartType(int value){
+		this.value = value;
+	}
+	
+	public int getValue(){
+		return this.value;
+	}
+}
