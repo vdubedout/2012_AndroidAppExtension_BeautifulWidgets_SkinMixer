e7faaeb42cc4c38a673c62c73b3086c472c98c84
Vincent Dubedout <vincent@dubedout.eu>
2012-10-03 20:43:27 +0200

    add name in label

--- SkinMixer/src/com/andexp/skinmixer/fragment/FragmentSkinPartList.java
+++ SkinMixer/src/com/andexp/skinmixer/fragment/FragmentSkinPartList.java
@@ -5,12 +5,17 @@ import java.util.ArrayList;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.andexp.skinmixer.R;
 import com.andexp.skinmixer.path.SkinLister;
 
 public class FragmentSkinPartList extends ListFragment implements OnSkinPartClickListener {
+	private TextView mLabelSkinName;
+
 	private SkinPartType mType;
 	private ArrayList<String> mSuperClockSkinPathList;
 
@@ -23,10 +28,22 @@ public class FragmentSkinPartList extends ListFragment implements OnSkinPartClic
 		getListView().setFocusable(false);
 	}
 
+	
 	public void setSkinPartType(SkinPartType skinPart) {
 		mType = skinPart;
 		setLayoutForType(mType);
+		mLabelSkinName = getLabelView(skinPart);
 	}
+
+	private TextView getLabelView(SkinPartType skinPart) {
+		LinearLayout parent = (LinearLayout) getView().getParent();
+		if(skinPart == SkinPartType.BACKGROUND){
+			return (TextView) parent.findViewById(R.id.skinmixer_backgroundlabel);
+		} else if (skinPart == SkinPartType.BACKGROUND_NUMBERS){
+			return (TextView) parent.findViewById(R.id.skinmixer_backgroundnumberslabel);
+		} else return (TextView) parent.findViewById(R.id.skinmixer_numberslabel);
+	}
+
 
 	private void setLayoutForType(SkinPartType skinPart) {
 		switch (skinPart) {
@@ -53,7 +70,8 @@ public class FragmentSkinPartList extends ListFragment implements OnSkinPartClic
 
 	@Override
 	public void OnSkinPartClick(int arrayPosition, View v) {
-		Toast.makeText(getActivity(), "click on " + mSuperClockSkinPathList.get(arrayPosition),
-				1000).show();
+		String[] splitResult = mSuperClockSkinPathList.get(arrayPosition).split("/");
+		String skinName = splitResult[splitResult.length-1];
+		mLabelSkinName.setText(skinName);
 	}
 }
