119db354e826b57899a0069de52b40290b573b7c
Vincent Dubedout <vincent@dubedout.eu>
2012-10-03 20:44:31 +0200

    format / clean

--- SkinMixer/src/com/andexp/skinmixer/fragment/FragmentSkinPartList.java
+++ SkinMixer/src/com/andexp/skinmixer/fragment/FragmentSkinPartList.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.andexp.skinmixer.R;
 import com.andexp.skinmixer.path.SkinLister;
