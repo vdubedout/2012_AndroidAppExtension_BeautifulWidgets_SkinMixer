5153605242f9a5278e90c351089fe2cf60f86634
Vincent Dubedout <vincent@dubedout.eu>
2012-10-03 18:36:14 +0200

    refactor, move methods, add labels initilization

--- SkinMixer/src/com/andexp/skinmixer/ActivitySkinMixer.java
+++ SkinMixer/src/com/andexp/skinmixer/ActivitySkinMixer.java
@@ -1,13 +1,17 @@
 package com.andexp.skinmixer;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
 import com.andexp.skinmixer.fragment.FragmentSkinPartList;
 import com.andexp.skinmixer.fragment.SkinPartType;
 
-import android.app.Activity;
-import android.app.FragmentManager;
-import android.os.Bundle;
-
 public class ActivitySkinMixer extends Activity {
+	View mBackgroundLabelView;
+	View mBackgroundNumbersLabelView;
+	View mNumbersLabelView;
+	
 	FragmentSkinPartList mBackgroundFragment;
 	FragmentSkinPartList mBackgroundNumbersFragment;
 	FragmentSkinPartList mNumbersFragment;
@@ -16,10 +20,21 @@ public class ActivitySkinMixer extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skinmixer);
-        
+        initializeLabelViews();
         initializeFragments();
-        
+        initializeOnClicksOnLabels();
     }
+
+	private void initializeLabelViews() {
+		mBackgroundLabelView = findViewById(R.id.skinmixer_backgroundlabel_layout);
+		mBackgroundNumbersLabelView = findViewById(R.id.skinmixer_backgroundnumberslabel_layout);
+		mNumbersLabelView = findViewById(R.id.skinmixer_numberslabel_layout);
+		
+	}
+
+	private void initializeOnClicksOnLabels() {
+		this.findViewById(R.id.skinmixer_backgroundlabel_layout).
+	}
 
 	private void initializeFragments() {
 		mBackgroundFragment = (FragmentSkinPartList) getFragmentManager().findFragmentById(R.id.skinmixer_fragment_background);
